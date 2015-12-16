02eca969a825a52e8753ff3209f20ecddc5222f6
Steph <lee.en.wei@gmail.com>
2015-12-15 22:17:51 -0800

    some bootstrap setup and improvement on CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resource :session, only: [:create, :destroy, :new]
-  resources :users, only: [:create, :new, :show]
+  resources :users, only: [:create, :new, :show, :index]
 
   namespace :api, defaults: { format: :json } do
     resources :spots
