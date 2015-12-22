7ecbf3dfb071c9d2fe63575a5bddc4e3e291eaeb
Steph <lee.en.wei@gmail.com>
2015-12-21 19:56:20 -0800

    migration error fixed for heroku

--- db/migrate/20151221225917_add_columnto_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddColumntoTags < ActiveRecord::Migration
-  def change
-    add_column :tags, :name, :string, null:false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221225917) do
+ActiveRecord::Schema.define(version: 20151221223524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,12 @@ ActiveRecord::Schema.define(version: 20151221225917) do
   add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
 
   create_table "tags", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "name",       null: false
   end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                                           null: false
