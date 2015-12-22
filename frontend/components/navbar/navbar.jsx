5bb528b7db94d1f5190c20c62e512703f35e69b8
Steph <lee.en.wei@gmail.com>
2015-12-22 09:21:50 -0800

    auth files, react navbar files created

---
+++ frontend/components/navbar/navbar.jsx
@@ -0,0 +1,18 @@
+var ReactDOM = require('react-dom');
+
+var NavBar = React.createClass({
+  render: function(){
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+          <div className="container-navbar">
+            <div className="navbar-header">
+      hi
+            </div>
+          </div>
+        </nav>
+
+    );
+  }
+});
+
+module.exports = NavBar;
